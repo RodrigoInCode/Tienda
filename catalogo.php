a7c3f9ae9b49bdda75833969d0c1ab6bdf748a84
Rodrigo <rodricampic@gmail.com>
2024-05-08 11:41:12 -0600

    Añadiendo servidor

--- catalogo.php
+++ catalogo.php
@@ -1,22 +1,6 @@
+<?php require('./layout/menu.php')?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="src/style.css">
-    
-    <title>Bebidas</title>
-</head>
-<body>
-    <header>
-        <h1>Bebidas</h1>
-    </header>
-
-    
-    <section class="contenedor">
+<section class="contenedor">
         <!-- Contenedor de elementos -->
         <div class="contenedor-items">
             <div class="item">
