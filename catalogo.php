7c29eb43c62bab4a3a284ec37d335c2731f3751d
Rodrigo <rodricampic@gmail.com>
2024-05-16 20:45:03 -0600

    Cambios en la organizacion

---
+++ catalogo.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="src/style.css">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <title>Bebidas</title>
+</head>
+
+<body>
+    <?php
+    include('./layout/menu.php');
+    ?>
+    <header>
+        <h1>Bebidas</h1>
+    </header>
+
+
+    <section class="contenedor">
+        <!-- Contenedor de elementos -->
+        <div class="contenedor-items">
+            <div class="item">
+                <span class="titulo-item">Ameyal fr/Kiwi 600</span>
+                <img src="src/imagenes/ameyal600.jpg" alt="" class="img-item">
+                <span class="precio-item">$18.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Ameyal 3L</span>
+                <img src="src/imagenes/ameyal3l.jpeg" alt="" class="img-item">
+                <span class="precio-item">$35.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Ameyal 600 ml</span>
+                <img src="src/imagenes/ameyal600.webp" alt="" class="img-item">
+                <span class="precio-item">$18.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Ameyal dura/man. 2 lts</span>
+                <img src="src/imagenes/durazno ameyal.webp" alt="" class="img-item">
+                <span class="precio-item">$21.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Ameyal fresa kiwi 2 lts</span>
+                <img src="src/imagenes/ameyal.jpg" alt="" class="img-item">
+                <span class="precio-item">$21.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Coca Cola 2.75 lts</span>
+                <img src="src/imagenes/coca2.70.jpeg" alt="" class="img-item">
+                <span class="precio-item">$48.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Coca Cola 400 ml</span>
+                <img src="src/imagenes/coca400.webp" alt="" class="img-item">
+                <span class="precio-item">$15.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Coca Cola 1.35 lts</span>
+                <img src="src/imagenes/coca1.3" alt="" class="img-item">
+                <span class="precio-item">$29.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+            <div class="item">
+                <span class="titulo-item">Coca Cola 1.75 lts NO Retornable</span>
+                <img src="src/imagenes/cocaola1.7.webp" alt="" class="img-item">
+                <span class="precio-item">$30.00</span>
+                <button class="boton-item">Agregar al Carrito</button>
+            </div>
+        </div>
+        <!-- Carrito de Compras -->
+        <div class="carrito" id="carrito">
+            <div class="header-carrito">
+                <h2>Tu Carrito</h2>
+            </div>
+
+            <div class="carrito-items">
+                <!-- 
+                <div class="carrito-item">
+                    <img src="img/boxengasse.png" width="80px" alt="">
+                    <div class="carrito-item-detalles">
+                        <span class="carrito-item-titulo">Box Engasse</span>
+                        <div class="selector-cantidad">
+                            <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="1" class="carrito-item-cantidad" disabled>
+                            <i class="fa-solid fa-plus sumar-cantidad"></i>
+                        </div>
+                        <span class="carrito-item-precio">$15.000,00</span>
+                    </div>
+                   <span class="btn-eliminar">
+                        <i class="fa-solid fa-trash"></i>
+                   </span>
+                </div>
+                <div class="carrito-item">
+                    <img src="img/skinglam.png" width="80px" alt="">
+                    <div class="carrito-item-detalles">
+                        <span class="carrito-item-titulo">Skin Glam</span>
+                        <div class="selector-cantidad">
+                            <i class="fa-solid fa-minus restar-cantidad"></i>
+                            <input type="text" value="3" class="carrito-item-cantidad" disabled>
+                            <i class="fa-solid fa-plus sumar-cantidad"></i>
+                        </div>
+                        <span class="carrito-item-precio">$18.000,00</span>
+                    </div>
+                   <button class="btn-eliminar">
+                        <i class="fa-solid fa-trash"></i>
+                   </button>
+                </div>
+                 -->
+            </div>
+            <div class="carrito-total">
+                <div class="fila">
+                    <strong>Tu Total</strong>
+                    <span class="carrito-precio-total">
+                        $120.000,00
+                    </span>
+                </div>
+                <button class="btn-pagar">Pagar <i class="fa-solid fa-bag-shopping"></i> </button>
+            </div>
+        </div>
+    </section>
+    <script src="public\bundle.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Super G</title>
+    <link rel="stylesheet" href="./public/bundle.css" />
+    <link rel="icon" type="image/x-icon" href="src/imagenes/logo_sin_fondp.png" />
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+</head>
+
+<body>
+    <?php
+    include('./layout/menu.php');
+    ?>
+    <main>
+        <div class="contenedor-main">
+            <div class="contenedor_head">
+                <img src="src/imagenes/logo_sin_letras (1).png" alt="" width="50px" height="50px" />
+                <div class="contenedor_head_texto">
+                    <p>Tienda Super G</p>
+                </div>
+                <div class="contenedor_head_boton">
+                    <span>Contactanos</span>
+                    <ion-icon name="arrow-forward-outline"></ion-icon>
+                </div>
+            </div>
+            <section id="seccion1">
+                <div class="contenedor_contenido">
+                    <p>
+                        Todo lo que buscas, <br />
+                        <span class="typed"></span>
+                    </p>
+                    <div class="parrafo_contenido">
+                        Lorem ipsum dolor sit amet consectetur <br> adipisicing elit. Numquam, beatae!
+                    </div>
+                    <div class="boton_contenido">
+                        <span>Pide ya</span>
+                    </div>
+                </div>
+            </section>
+            <section id="seccion2">
+                <div class="contenedor-info" id="info">
+                    <h3>Sobre Nosotros</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                        Sit modi iste dolores architecto? Odio asperiores iure quibusdam itaque <br>
+                        voluptas ad est reiciendis ipsum, pariatur, rerum nobis! Distinctio necessitatibus <br>
+                        dolorum accusantium.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>
+                        Sit modi iste dolores architecto? Odio asperiores iure quibusdam itaque <br>
+                        voluptas ad est reiciendis ipsum, pariatur, rerum nobis! Distinctio necessitatibus <br>
+                        dolorum accusantium.</p>
+                    <img src="./src/assets/compra_online.svg" id="imagen-info">
+                </div>
+            </section>
+            <section id="seccion3">
+                <div class="contenedor_comentarios" id="com">
+                    <div class="slider-contenedor">
+                        <div class="slider-test">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nulla.</p>
+                            <img src="./src/imagenes/imagen-comentario.jpg" alt="">
+                            <h3>Nombre1</h3>
+                        </div>
+                        <div class="slider-test">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nulla.</p>
+                            <img src="./src/imagenes/imagen-comentario.jpg" alt="">
+                            <h3>Nombre2</h3>
+                        </div>
+                        <div class="slider-test">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, nulla.</p>
+                            <img src="./src/imagenes/imagen-comentario.jpg" alt="">
+                            <h3>Nombre3</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="seccion4">
+                <h1>Nuestra Ubicacion</h1>
+                <div class="contenedor-map" id="map">
+
+                    <div id="map" style="height: 800px;">
+
+                    </div>
+                </div>
+            </section>
+        </div>
+        </div>
+    </main>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    <script src="./public/bundle.js"></script>
+</body>
+
+</html>
